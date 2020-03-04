a746f4af759d72bc70d8cbc84ea90906c5c9ffec
Anthony Joseph Tran <ajt025@ucsd.edu>
2020-03-03 22:55:09 -0800

    Initial Commit

---
+++ BeachSimulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BeachSimulator
+//
+//  Created by Anthony Joseph Tran on 3/3/20.
+//  Copyright © 2020 Anthony Joseph Tran. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
